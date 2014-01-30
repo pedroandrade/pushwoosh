a2d222214c8d1a83101734a6e90bde5263cedd60
Pedro Andrade <pedro.rjandrade@gmail.com>
2014-01-30 13:45:06 -0300

    - Refactor: better handling of errors - Code improvement - Accept notification options

--- lib/pushwoosh/push_notification.rb
+++ lib/pushwoosh/push_notification.rb
@@ -2,31 +2,59 @@ require 'httparty'
 require 'json'
 require 'pushwoosh/request'
 require 'pushwoosh/response'
+require 'active_support/core_ext/hash/indifferent_access'
+require 'pushwoosh/helpers'
 
 module Pushwoosh
   class PushNotification
 
+    class Error < StandardError; end;
+
     STRING_BYTE_LIMIT = 256
 
     def initialize(options = {})
-      @auth = {application: options[:application], auth: options[:auth]}
+      fail 'Missing application' unless options[:application]
+      fail 'Missing auth key' unless options[:auth]
+
+      @base_request = {
+        request: {
+          application: options[:application],
+          auth: options[:auth]
+        }
+      }
     end
 
-    def notify_all(message)
-      create_message({content: Pushwoosh::Helpers::limit_string(message, STRING_BYTE_LIMIT)})
+    def notify_all(message, options= {})
+      options.merge!(content: Helpers.limit_string(message, STRING_BYTE_LIMIT))
+      create_message(options)
     end
 
-    def notify_devices(message, devices)
-      create_message({content: Pushwoosh::Helpers::limit_string(message, STRING_BYTE_LIMIT), devices: devices})
+    def notify_devices(message, devices, options = {})
+      create_message(
+        content: Helpers.limit_string(message, STRING_BYTE_LIMIT),
+        devices: devices
+      )
     end
+
+    private
 
     def create_message(notification_options = {})
-     default_notification_options = {send_date: "now"}
-     final_notification_options = default_notification_options.merge(notification_options)
-     options = @auth.merge({notifications: [final_notification_options]})
-     options = {request:options}
-     response = Request.post("/createMessage",  body: options.to_json)
-     Response.new(response.parsed_response['status_code'], response.parsed_response['status_message'], response.parsed_response['response'])
+      fail Error, 'Message is missing' if notification_options[:content].empty?
+      response = Request.post("/createMessage",
+        body: build_request(notification_options))
+     Response.new(response.parsed_response.with_indifferent_access)
+    end
+
+    def build_request(notification_options = {})
+      @base_request.merge(notifications:
+        [default_notification_options.merge(notification_options)])
+    end
+
+    def default_notification_options
+      {
+        send_date: "now",
+        ios_bagdes: "+1"
+      }
     end
   end
 end
