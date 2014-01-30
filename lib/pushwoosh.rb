f1ac8143b49ef14298ee9e4b6fb6b146b347e0dc
Pedro Andrade <pedro.rjandrade@gmail.com>
2014-01-30 13:46:25 -0300

    -Accept notification options

--- lib/pushwoosh.rb
+++ lib/pushwoosh.rb
@@ -2,19 +2,19 @@ require "pushwoosh/version"
 require 'pushwoosh/push_notification'
 require 'pushwoosh/configurable'
 require 'httparty'
-require 'active_support/all'
 require 'pushwoosh/helpers'
 
 module Pushwoosh
+
   class << self
     include Pushwoosh::Configurable
 
-    def notify_all(message)
-      response = PushNotification.new(options).notify_all(message)
+    def notify_all(message, notification_options = {})
+      response = PushNotification.new(options).notify_all(message, notification_options)
     end
 
-    def notify_devices(message, devices = [])
-      response = PushNotification.new(options).notify_devices(message, devices)
+    def notify_devices(message, devices = [], notification_options = {})
+      response = PushNotification.new(options).notify_devices(message, devices, notification_options)
     end
 
   end
