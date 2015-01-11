5de2c70e665b1bf7797602b5bf3227cbc969cc61
Pedro Andrade <pedro.rjandrade@gmail.com>
2015-01-11 18:49:31 -0300

    fix undefined method `force_encoding' for nil:NilClass

--- lib/pushwoosh/helpers.rb
+++ lib/pushwoosh/helpers.rb
@@ -2,13 +2,16 @@ module Pushwoosh
   module Helpers
     class << self
       def limit_string(str, limit_bytes)
-         str = str.mb_chars.compose.to_s if str.respond_to?(:mb_chars)
-         new_str = str.byteslice(0, limit_bytes)
-         until new_str[-1].force_encoding('utf-8').valid_encoding?
-           # remove the invalid char
-           new_str = new_str.slice(0..-2)
-         end
-         new_str
+        return '' if str.empty? || !str
+
+        str = str.mb_chars.compose.to_s if str.respond_to?(:mb_chars)
+        new_str = str.byteslice(0, limit_bytes)
+        until new_str[-1].force_encoding('utf-8').valid_encoding?
+          # remove the invalid char
+          new_str = new_str.slice(0..-2)
+        end
+
+        new_str
       end
     end
   end
