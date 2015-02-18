e3d451b3045e4e965230e101e5bcf74dd45c2f3c
Antonio Antillon <antillas21@gmail.com>
2015-02-18 14:12:26 -0800

    Edits Pushwoosh::Response to inherit from OpenStruct
    This saves us lines of code, and we don't to manually specify the
    attrs that a Pushwoosh::Response object responds to, as they're
    the same attrs returned by the API call.

--- lib/pushwoosh/response.rb
+++ lib/pushwoosh/response.rb
@@ -1,12 +1,5 @@
+require 'ostruct'
+
 module Pushwoosh
-  class Response
-
-    attr_accessor :status_code, :status_message, :response
-
-    def initialize(pushwoosh_response = {})
-      @status_code = pushwoosh_response[:status_code]
-      @status_message = pushwoosh_response[:status_message]
-      @response = pushwoosh_response[:response]
-    end
-  end
+  class Response < OpenStruct; end
 end
