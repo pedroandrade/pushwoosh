40a183965a8a4d73b7566271e2c62cf6456f271c
Andrew Emelianenko <emelianenko.web@gmail.com>
2015-09-25 17:40:54 +0300

    Update tests for Silent notifications

--- spec/pushwoosh/pushwoosh_spec.rb
+++ spec/pushwoosh/pushwoosh_spec.rb
@@ -19,9 +19,18 @@ describe Pushwoosh do
     end
 
     context 'when message is empty' do
-      it 'raises a error if message is empty' do
+      it 'send silent push if message is empty' do
         VCR.use_cassette 'pushwoosh/empty_message_push' do
-          expect { described_class.notify_all('') }.to raise_error
+          response = described_class.notify_all('')
+          expect(response.status_code).to eq 200
+        end
+      end
+    end
+
+    context 'when message is nil' do
+      it 'raises a error if message is nil' do
+        VCR.use_cassette 'pushwoosh/nil_message_push' do
+          expect { described_class.notify_all(nil) }.to raise_error
         end
       end
     end
