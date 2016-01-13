01d9a6415cc2f7305422bac94862ffe27098b590
Jon Maddox <jon@jonmaddox.com>
2016-01-13 10:25:24 -0500

    This isn't needed anymore

--- lib/pushwoosh/helpers.rb
+++ lib/pushwoosh/helpers.rb
@@ -1,18 +1,6 @@
 module Pushwoosh
   module Helpers
     class << self
-      def limit_string(str, limit_bytes)
-        return '' if str.empty? || !str
-
-        str = str.mb_chars.compose.to_s if str.respond_to?(:mb_chars)
-        new_str = str.byteslice(0, limit_bytes)
-        until new_str[-1].force_encoding('utf-8').valid_encoding?
-          # remove the invalid char
-          new_str = new_str.slice(0..-2)
-        end
-
-        new_str
-      end
     end
   end
 end
--- spec/pushwoosh/helpers_spec.rb
+++ spec/pushwoosh/helpers_spec.rb
@@ -1,12 +1,4 @@
 require 'spec_helper'
 
 describe Pushwoosh::Helpers do
-  describe '.limit_string' do
-    context 'when string is bigger than 256 bytes' do
-      it 'returns the string with 256 bytes' do
-        big_string = 'é' * 300
-        expect(described_class.limit_string(big_string, 256).bytes.size).to eq 256
-      end
-    end
-  end
 end
